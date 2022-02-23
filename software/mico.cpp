42e9213c18328e571e02e4adad450e824bf58abc
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2022-02-23 11:57:46 +0100

    make it work with some of the hardware

--- software/mico.cpp
+++ software/mico.cpp
@@ -15,10 +15,14 @@ using namespace pimoroni;
 
 #define LED_PIN PICO_DEFAULT_LED_PIN
 
-constexpr auto pinA = 12;
-constexpr auto pinB = 13;
-PIO pio = pio0;
-Encoder enc1(pio, pinA, pinB);
+// Pins are GPIO pin numbers, not physical pins
+// PIO pio = pio0;
+Encoder enc1(pio0, 2, 3);
+Encoder enc2(pio1, 4, 5);
+
+// FIXME: These don't work. NEEDZ MORE PIO
+Encoder enc3(pio1, 6, 7);
+Encoder enc4(pio1, 8, 9);
 
 // UART defines
 // By default the stdout UART is `uart0`, so we will use the second one
@@ -28,12 +32,15 @@ Encoder enc1(pio, pinA, pinB);
 // Use pins 4 and 5 for UART1
 // Pins can be changed, see the GPIO function select table in the datasheet for
 // information on GPIO assignments
-#define UART_TX_PIN 4
+#define UART_TX_PIN 14
 #define UART_RX_PIN 5
 
 inline void setup(){
 
   enc1.init();
+  enc2.init();
+  enc3.init();
+  enc4.init();
 
   // Turn on LED
   gpio_init(LED_PIN);
@@ -60,15 +67,17 @@ int main() {
   // gpio_pull_up(GPIO);
 
   while (true) {
-	auto capture = enc1.perform_capture();
-	auto count = enc1.get_count();
-	// printf("Hello, world!\n");
-	printf("count: %d\n", count);
-	printf("state a: %d\n", enc1.get_state_a());
-	printf("state a: %d\n", enc1.get_state_a());
-	// printf("state b: %d", enc1.get_state_b());
-	// printf("%d\n", count);
-	sleep_ms(100);
+	auto count1 = enc1.get_count();
+	auto count2 = enc2.get_count();
+	auto count3 = enc3.get_count();
+	auto count4 = enc4.get_count();
+
+	printf("count1: %d\n", count1);
+	printf("count2: %d\n", count2);
+	printf("count3: %d\n", count3);
+	printf("count4: %d\n", count4);
+
+	sleep_ms(200);
   }
 
   return 0;
