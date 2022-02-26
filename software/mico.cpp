91ee040ad4c7ee43d2f45ab353094a6579f5f016
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2022-02-26 16:31:07 +0100

    basic 8 bit midi cc support added

--- software/mico.cpp
+++ software/mico.cpp
@@ -1,18 +1,12 @@
-// #include "hardware/clocks.h"
-// #include "hardware/divider.h"
 #include "hardware/gpio.h"
 #include "hardware/interp.h"
-// #include "hardware/pio.h"
-// #include "hardware/timer.h"
-// #include "hardware/uart.h"
-// #include "hardware/watchdog.h"
-#include "pico/stdlib.h"
 #include "hardware/pio.h"
-#include <stdio.h>
 #include "libs/encoder-pio/pioencoder.hpp"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
-#include "pico/binary_info.h"
 #include "bsp/board.h"
+#include "pico/binary_info.h"
 #include "tusb.h"
 
 using namespace pimoroni;
@@ -35,10 +29,11 @@ PioEncoder enc3(pio1, 8, 9);
 // Use pins 4 and 5 for UART1
 // Pins can be changed, see the GPIO function select table in the datasheet for
 // information on GPIO assignments
+// FIXME: Will this work ? Is it neceassry?
 #define UART_TX_PIN 14
-#define UART_RX_PIN 5
+#define UART_RX_PIN 15
 
-inline void setup(){
+inline void setup() {
 
   enc1.init();
   enc2.init();
@@ -49,13 +44,84 @@ inline void setup(){
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
   gpio_put(LED_PIN, 1);
+}
 
+// Creates a 4 byte midi buffer and sends it as a control change midi message.
+// channel is midi channel. 0 = channel 1.
+// value is the midi value
+// ccnum is control change number
+// cable_num is MIDI jack associated with USB endpoint
+// Inspired by
+// https://github.com/arduino-libraries/MIDIUSB/blob/master/examples/MIDIUSB_write/MIDIUSB_write.ino
+void send_cc(uint8_t channel, uint8_t value, uint8_t ccnum,
+             uint8_t cable_num = 0) {
+  constexpr auto size = 4;
+  // First parameter is the event type (0x0B = control change).
+  // Second parameter is the event type, combined with the channel.
+  // Third parameter is the control number number (0-119).
+  // Fourth parameter is the control value (0-127).
+  // TODO: Should this be a struct instead? Would that be more compiler
+  // friendly?
+  const uint8_t ccbuffer[size] = {0x0B, static_cast<uint8_t>(0xB0 | channel),
+                                  ccnum, value};
+  tud_midi_stream_write(cable_num, ccbuffer, size);
+}
+
+void send_cc14(uint8_t channel, uint8_t value, uint8_t ccnum,
+               uint8_t cable_num = 0) {
+  // TODO
+}
+
+int32_t old_count1{0}, old_count2{0}, old_count3{0}, old_count4{0};
+
+void midi_task(void) {
+  // TODO: This whole function is terribly ugly... oh well.
+  auto chan = 0;
+
+  auto count1 = enc1.get_count();
+  if (count1 != old_count1) {
+    old_count1 = count1;
+
+    auto ccnum = 1;
+    auto val = (count1 < 0)? 0 : count1 % 127;
+
+    send_cc(chan, val, ccnum);
+  };
+
+  auto count2 = enc2.get_count();
+  if (count2 != old_count2) {
+    old_count2 = count2;
+
+    auto ccnum = 2;
+    auto val = count2 % 127;
+    send_cc(chan, val, ccnum);
+  };
+
+  auto count3 = enc3.get_count();
+  if (count3 != old_count3) {
+    old_count3 = count3;
+
+    auto ccnum = 3;
+    auto val = count3 % 127;
+    send_cc(chan, val, ccnum);
+  };
+
+  auto count4 = enc4.get_count();
+  if (count4 != old_count4) {
+    old_count4 = count4;
+
+    auto ccnum = 4;
+    auto val = count4 % 127;
+    send_cc(chan, val, ccnum);
+  };
 }
 
 int main() {
   stdio_init_all();
   tusb_init();
   setup();
+  // TODO: Is this already done by Pico sdk?
+  board_init();
 
   // Set up our UART
   // uart_init(UART_ID, BAUD_RATE);
@@ -71,20 +137,9 @@ int main() {
   // gpio_pull_up(GPIO);
 
   while (true) {
-	tud_task(); // tinyusb device task
-
-	auto count1 = enc1.get_count();
-	auto count2 = enc2.get_count();
-	auto count3 = enc3.get_count();
-	auto count4 = enc4.get_count();
-
-	printf("count1: %d\n", count1);
-	printf("count2: %d\n", count2);
-	printf("count3: %d\n", count3);
-	printf("count4: %d\n", count4);
-
-	sleep_ms(10);
-
+    tud_task(); // tinyusb device task
+    midi_task();
+    sleep_ms(1);
   }
 
   return 0;
